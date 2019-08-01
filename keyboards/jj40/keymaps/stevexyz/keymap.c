be447d0b0e5c2189b6f2d8e088a30034cd9db070
steve <steve@stery>
2019-08-01 09:11:12 +0200

    IGNORE_MOD_TAP_INTERRUPT and other small changes

--- keyboards/jj40/keymaps/stevexyz/keymap.c
+++ keyboards/jj40/keymaps/stevexyz/keymap.c
@@ -14,14 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
-http://www.keyboard-layout-editor.com/#/
-[{c:"#eeddcc",g:true,a:7},"","","","","","","","","","","",""],
-["",{c:"#cccccc",g:false,a:4,fa:[8,1,1,2]},"Q\nF1\nF11\n1\n\n\n\n!","W\nF2\nF12\n2\n\n\n\n@","E\nF3\n\n3\n\n\n\n#","R\nF4\n<i class='fa fa-lightbulb-o'></i>pad\n4\n\n\n\n$","T\nF5\nnpad\n5\n\n\n\n%","Y\nF6\nfn<i class='fa fa-lock'></i>\n6\n\n\n\n^","U\nF7\n<i class='fa fa-photo'></i>\n7\n\n\n\n&","I\nF8\n<i class='fa fa-arrows'></i>\n8\n\n\n\n*","O\nF9\n<i class='fa fa-pause'></i>\n9\n\n\n\n(","P\nF10\n\n0\n\n\n\n)",{c:"#eeddcc",g:true,a:7},""],
-["",{c:"#cccccc",g:false,a:4,fa:[8,2,1,2]},"A\n<i class='fa fa-mail-reply'></i>\n<i class='fa fa-level-up'></i>\n<i class='fa fa-long-arrow-right'></i>","S\n<i class='fa fa-angle-double-left'></i>\n<i class='fa fa-volume-off'></i>\n`\n\n\n\n~","D\n<i class='fa fa-angle-double-down'></i>\n<i class='fa fa-volume-down'></i>\n-\n\n\n\n_","F\n<i class='fa fa-angle-double-up'></i>\n<i class='fa fa-volume-up'></i>\n=\n\n\n\n+","G\n<i class='fa fa-angle-double-right'></i>\n<i class='fa fa-play'></i>\n[\n\n\n\n{","H\n<i class='fa fa-arrow-left'></i>\n<i class='fa fa-step-forward'></i>\n]\n\n\n\n}","J\n<i class='fa fa-arrow-down'></i>\n\n\\\n\n\n\n|","K\n<i class='fa fa-arrow-up'></i>\n<i class='fa fa-music'></i>\n;\n\n\n\n:","L\n<i class='fa fa-arrow-right'></i>\n<i class='fa fa-mouse-pointer'></i>\n'\n\n\n\n\"",{c:"#ff7878",f:1},"\n<i class='fa fa-long-arrow-left'></i>\nIns\nDel",{c:"#eeddcc",g:true,a:7,f:3},""],
-["","",{c:"#cccccc",t:"#000000\n\n\n\n\n\n\n\n\n\n#804020",g:false,a:4,fa:[8,0,0,0,0,0,0,0,0,0,1]},"Z\n\n\n\n\n\n\n\n\n\nctrl","X\n\n\n\n\n\n\n\n\n\nalt",{t:"#000000\n#804020",fa:[8,1,1]},"C\nfn\n^",{t:"#000000\n\n\n#804020",fa:[8,1,1,1]},"V\n\n^\nnum",{t:"#000000\n#804020",a:5},"\nshift",{t:"#000000\n\n\n\n\n\n\n\n\n\n#804020",a:4,fa:[8,1,1,2,0,0,0,0,0,0,1]},"B\n\n\n,\n\n\n\n<\n\n\n<i class='fa fa-apple'></i>","N\n\n\n.\n\n\n\n>\n\n\nalt","M\n\n\n/\n\n\n\n?\n\n\nctrl",{c:"#eeddcc",t:"#000000",g:true,a:7},"",""]
-*/
-
 #include QMK_KEYBOARD_H
 
 enum layers {
